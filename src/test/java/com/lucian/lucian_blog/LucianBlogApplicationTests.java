65d07bdb4e98a2371f7b4a3cde4e00fcdb2e2ffc
lingxiang deng <17607003651@163.com>
2021-10-18 22:57:34 +0800

    Initial commit

---
+++ src/test/java/com/lucian/lucian_blog/LucianBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucian.lucian_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucianBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
