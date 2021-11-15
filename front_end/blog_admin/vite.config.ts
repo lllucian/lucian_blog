d3023a6adb7247517a19520b4a3be2e037125329
denglingxiang <17607003651@163.com>
2021-11-15 21:34:29 +0800

    管理界面前端初始化

---
+++ front_end/blog_admin/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir);
+}
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: /\/@\//,
+        replacement: pathResolve('src') + '/',
+      }
+    ]
+  }
+})
