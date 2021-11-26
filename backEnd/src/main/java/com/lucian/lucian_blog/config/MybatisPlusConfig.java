f78059b708b402044a6c379ae5885a4c42800a02
denglingxinag <17607003651@163.com>
2021-11-26 08:47:10 +0800

    增加对特殊字符like查询的转义

--- backEnd/src/main/java/com/lucian/lucian_blog/config/MybatisPlusConfig.java
+++ backEnd/src/main/java/com/lucian/lucian_blog/config/MybatisPlusConfig.java
@@ -3,6 +3,7 @@ package com.lucian.lucian_blog.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.lucian.lucian_blog.interceptor.MybatisSpecialSymbolInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,7 @@ public class MybatisPlusConfig {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        interceptor.addInnerInterceptor(new MybatisSpecialSymbolInterceptor());
         return interceptor;
     }
 }
