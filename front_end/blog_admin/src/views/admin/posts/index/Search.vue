8d90edc5fa53aacd3fab5faf6c01b8235ad517af
denglingxinag <17607003651@163.com>
2021-11-26 15:29:44 +0800

    删除多余代码

--- front_end/blog_admin/src/views/admin/posts/index/Search.vue
+++ front_end/blog_admin/src/views/admin/posts/index/Search.vue
@@ -105,40 +105,6 @@ export default defineComponent({
       SearchForm.value.resetFields();
     };
 
-    const shortcuts = [
-      {
-        text: "上周",
-        value: () => {
-          const end = new Date();
-          const start = new Date();
-          start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-          return [start, end];
-        },
-      },
-      {
-        text: "上个月",
-        value: () => {
-          const end = new Date();
-          const start = new Date();
-          start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-          return [start, end];
-        },
-      },
-      {
-        text: "3个月前",
-        value: () => {
-          const end = new Date();
-          const start = new Date();
-          start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
-          return [start, end];
-        },
-      },
-    ];
-
-    const defaultTime = [
-      new Date(2000, 1, 1, 12, 0, 0),
-      new Date(2000, 2, 1, 23, 59, 59),
-    ]
     const categoryRemoteMethod = async (query: string) => {
       if (query.trim() !== "") {
         categoryLoading.value = true;
