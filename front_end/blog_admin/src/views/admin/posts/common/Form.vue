e28196e34a277060f78fa75df56f1e461a0d6ef5
denglingxinag <17607003651@163.com>
2021-12-01 08:57:17 +0800

    post form表单

--- front_end/blog_admin/src/views/admin/posts/common/Form.vue
+++ front_end/blog_admin/src/views/admin/posts/common/Form.vue
@@ -1,9 +1,9 @@
 <template>
-  <el-page-header content="文章一览" @back="goBack" />
+  <el-page-header content="文章一览" @back="goBack"/>
   <el-card>
     <template #header>
       <div class="card-header">
-        <h4>{{title}}</h4>
+        <h4>{{ title }}</h4>
       </div>
     </template>
     <el-form ref="form" :model="formData" :rules="rules" label-width="120px" label-position="top">
@@ -16,6 +16,34 @@
       <el-form-item label="内容" prop="content">
         <TEditor ref="editor" v-model="formData.content"/>
       </el-form-item>
+      <el-form-item label="分类" prop="categories">
+        <el-select-v2
+            v-model="formData.categories"
+            filterable
+            :options="categoryOptions"
+            remote
+            :remote-method="categoryRemoteMethod"
+            :loading="categoryLoading"
+            style="width: 100%"
+            placeholder="请选择分类"
+            multiple
+            clearable
+        />
+      </el-form-item>
+      <el-form-item label="标签" prop="tags">
+        <el-select-v2
+            v-model="formData.tags"
+            filterable
+            :options="tagOptions"
+            remote
+            :remote-method="tagRemoteMethod"
+            :loading="tagLoading"
+            style="width: 100%"
+            placeholder="请选择标签"
+            multiple
+            clearable
+        />
+      </el-form-item>
     </el-form>
   </el-card>
 </template>
@@ -23,6 +51,7 @@
 <script lang="ts">
 import {defineComponent, ref, toRefs} from "vue";
 import {router} from "/@/router";
+import {postRequest} from "/@/requests";
 
 export default defineComponent({
   props: {
@@ -32,7 +61,7 @@ export default defineComponent({
     }
   },
   setup(props) {
-    const { title } = toRefs(props);
+    const {title} = toRefs(props);
     const formData = ref({
       title: '',
       description: '',
@@ -45,17 +74,69 @@ export default defineComponent({
 
     const rules = ref({
       title: [
-        {required: true, message: "标题必填", trigger: "blur" }
+        {required: true, message: "标题必填", trigger: "blur"},
+        {max: 255, message: '标题最长255个字符'}
       ],
       content: [
         {required: true, message: "内容必填", trigger: "change"}
+      ],
+      categories: [
+        {required: true, message: "分类必须"}
       ]
     });
+
+    const categoryLoading = ref(false);
+
+    const categoryOptions = ref([]);
+
+    const categoryRemoteMethod = async (query: string) => {
+      if (query.trim() !== "") {
+        categoryLoading.value = true;
+        try {
+          const data = postRequest("api/admin/fetch/categories", {name: query});
+          if ((await data).data) categoryOptions.value = (await data).data;
+        } finally {
+          categoryLoading.value = false;
+        }
+      } else {
+        categoryOptions.value = [];
+      }
+    }
+
+    const tagLoading = ref(false);
+
+    const tagOptions = ref([]);
+
+    const tagRemoteMethod = async (query: string) => {
+      if (query.trim() !== "") {
+        tagLoading.value = true;
+        try {
+          const data = postRequest("api/admin/fetch/tags", {name: query});
+          if ((await data).data) tagOptions.value = (await data).data;
+        } finally {
+          tagLoading.value = false;
+        }
+      } else {
+        tagOptions.value = [];
+      }
+    }
 
     const goBack = () => {
       router.push({name: 'AdminPostIndex'})
     }
-    return {form, formData, title, goBack, rules}
+    return {
+      form,
+      formData,
+      title,
+      goBack,
+      rules,
+      categoryLoading,
+      categoryOptions,
+      categoryRemoteMethod,
+      tagLoading,
+      tagOptions,
+      tagRemoteMethod,
+    }
   }
 })
 </script>
