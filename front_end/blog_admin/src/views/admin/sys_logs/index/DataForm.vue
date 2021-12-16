5fc5ef919431c2ab271d145051c6bdf13d3a0084
denglingxiang <17607003651@163.com>
2021-12-16 14:26:22 +0800

    日志管理一览

---
+++ front_end/blog_admin/src/views/admin/sys_logs/index/DataForm.vue
@@ -0,0 +1,136 @@
+<template>
+  <el-collapse model-value="DataForm" accordion v-loading="loadingTable">
+    <el-collapse-item name="DataForm">
+      <template #title>
+        <span
+          class="iconify"
+          data-icon="ant-design:ordered-list-outlined"
+        ></span>
+        检索数据
+      </template>
+      <el-table :data="dataTable" :border="true" stripe>
+        <el-table-column
+          type="selection"
+          width="55"
+          align="center"
+          header-align="center"
+        />
+        <el-table-column prop="username" label="操作者" width="140" header-align="center"></el-table-column>
+        <el-table-column prop="operation" label="操作" width="100" header-align="center"> </el-table-column>
+        <el-table-column prop="time" label="所用时间"  width="100" header-align="center"> </el-table-column>
+        <el-table-column prop="method" label="请求方法"  width="200" header-align="center"> </el-table-column>
+        <el-table-column prop="requestParams" label="请求参数" header-align="center"> </el-table-column>
+        <el-table-column prop="responseStatus" label="返回状态"  header-align="center"> </el-table-column>
+        <el-table-column prop="ip" label="IP" width="100" align="center"> </el-table-column>
+        <el-table-column prop="address" label="地址" align="center"></el-table-column>
+        <el-table-column prop="browser" label="浏览器" align="center"></el-table-column>
+        <el-table-column prop="os" label="系统" align="center"></el-table-column>
+      </el-table>
+      <Pagination
+        v-model:currentPage="pageInfo.current"
+        v-model:pageSize="pageInfo.size"
+        v-model:total="pageInfo.total"
+        @pagination="getList"
+      ></Pagination>
+    </el-collapse-item>
+  </el-collapse>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted, ref, toRefs, watch } from "vue";
+import Pagination from "/@/components/data/Pagination.vue";
+import { setPageConditions, apiFormData } from "./query";
+import Icon from "/@/components/basic/Icon.vue"
+
+export default defineComponent({
+  components: { Pagination, Icon },
+  props: {
+    dataTable: {
+      type: Array,
+      default: [],
+      required: false,
+    },
+    total: {
+      type: Number,
+      default: 0,
+      required: false,
+    },
+    size: {
+      type: Number,
+      default: 10,
+      required: false,
+    },
+    current: {
+      type: Number,
+      default: 1,
+      required: false,
+    },
+    loadingTable: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+  },
+  emits: [
+    "update:dataTable",
+    "update:size",
+    "update:total",
+    "update:current",
+    "update:loadingTable",
+  ],
+  setup(props, { emit }) {
+    const pageInfo = ref({
+      current: 1,
+      size: 10,
+      total: 0,
+    });
+
+    const { dataTable, total, current, size, loadingTable } = toRefs(props);
+
+    onMounted(() => {
+      getList();
+    });
+
+    watch(total, (newValue, oldValue) => {
+      pageInfo.value.total = newValue;
+    });
+
+    watch(current, (newValue, oldValue) => {
+      pageInfo.value.current = newValue;
+    });
+
+    watch(size, (newValue, oldValue) => {
+      pageInfo.value.size = newValue;
+    });
+
+    const getList = async () => {
+      setPageConditions(pageInfo.value);
+      emit("update:loadingTable", true);
+      try {
+        const { records, current, size, total } = (pageInfo.value =
+          await apiFormData());
+        pageInfo.value.current = current;
+        pageInfo.value.size = size;
+        pageInfo.value.total = total;
+        emit("update:dataTable", records);
+        emit("update:current", current);
+        emit("update:size", size);
+        emit("update:total", total);
+      } finally {
+        emit("update:loadingTable", false);
+      }
+    };
+
+    return { dataTable, pageInfo, getList, loadingTable };
+  },
+});
+</script>
+<style scoped>
+.el-collapse {
+  padding: 8px;
+  background: white;
+}
+
+.el-tag + .el-tag {
+  margin-left: 10px;
+}
+</style>
---
+++ front_end/blog_admin/src/views/admin/sys_logs/index/query.ts
@@ -0,0 +1,48 @@
+import { postRequest } from "/@/requests";
+
+export interface pageInfo {
+    size: number;
+    current: number;
+    total: number;
+}
+export interface queryConditionsByPageInterface extends pageInfo{
+    operation: string | null;
+    userId: number | null;
+    method: string | null;
+    size: number;
+    current: number;
+    total: number;
+    records: any[];
+  }
+
+export let queryConditionsByPage: queryConditionsByPageInterface;
+
+export const setQueryConditions = (
+  conditions: queryConditionsByPageInterface
+) => {
+  queryConditionsByPage = conditions;
+};
+
+export const setPageConditions = (pageInfo: pageInfo) => {
+    queryConditionsByPage = {...queryConditionsByPage, ...pageInfo}
+}
+
+export const getPagesize = () => {
+    return queryConditionsByPage.size || 10;
+}
+
+
+export const apiFormData = async () => {
+    const data = await postRequest("api/admin/sys_logs", queryConditionsByPage);
+
+    if(data.data && data.data.records){
+        return {
+            records: data.data.records,
+            total: data.data.total,
+            current: data.data.current,
+            size: data.data.size,
+        };
+    } else {
+        return {records: [], total: 0, current: 1, size: data.data.size};
+    }
+}
