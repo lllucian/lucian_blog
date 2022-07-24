360e164a73b68519f3fc012f2dfd03aa9ca3b26c
denglingxiang <17607003651@163.com>
2022-07-24 18:07:45 +0800

    slider代码修改

--- front_end/blog_admin/src/views/front/home/Slider.vue
+++ front_end/blog_admin/src/views/front/home/Slider.vue
@@ -2,23 +2,26 @@
   <Carousel :wrap-around="true" :autoplay="autoplay" v-if="sliders.length!==0">
     <Slide v-for="(slider, index) in sliders" :key="index">
       <div class="carousel__item" :style="{height: pageHeight}">
-          <div class="row slider-text justify-content-center align-items-center" style="width: 100%;">
-            <div class="col-md-6 text">
-              <div class="author mb-4 d-flex align-items-center">
-                <a href="#" class="img" :style="{backgroundImage: `url('${slider.imageUrl}')`}"></a>
-                <div class="ml-3 info">
-                  <span>Written by</span>
-                  <h3><a href="#">{{slider.username}}</a>, <span>{{slider.createdAt}}</span></h3>
-                </div>
-              </div>
-              <div class="text-2">
-                <span class="big">{{slider.username}}</span>
-                <h1 class="mb-4"><a href="#">{{slider.title}}</a></h1>
-                <p class="mb-4">{{slider.description}}</p>
-                <p><a href="#" class="btn btn-primary p-3 px-xl-4 py-xl-3">Continue Reading</a></p>
+        <div class="row slider-text justify-content-center align-items-center" style="width: 100%;">
+          <div class="col-md-6 text">
+            <div class="author mb-4 d-flex align-items-center">
+              <a href="#" class="img" :style="{backgroundImage: `url('${slider.imageUrl}')`}"></a>
+              <div class="ml-3 info">
+                <span>Written by</span>
+                <h3><a href="#">{{ slider.username }}</a>, <span>{{ slider.createdAt }}</span></h3>
               </div>
             </div>
-            <div class="img col-md-6" :style="{backgroundImage: `url(${slider.imageUrl})`, width: '100%', height: pageHeight}"></div>
+            <div class="text-2">
+              <span class="big">{{ slider.username }}</span>
+              <h1 class="mb-4"><a href="#">{{ slider.title }}</a></h1>
+              <p class="mb-4">{{ slider.description }}</p>
+              <p>
+                <router-link :to="`/post/${slider.id}`" class="btn btn-primary p-3 px-xl-4 py-xl-3">继续阅读</router-link>
+              </p>
+            </div>
+          </div>
+          <div class="img col-md-6"
+               :style="{backgroundImage: `url(${slider.imageUrl})`, width: '100%', height: pageHeight}"></div>
         </div>
 
       </div>
@@ -63,7 +66,7 @@ export default defineComponent({
 
     const loadData = async () => {
       const data = await getRequest("/sliders");
-      if (data && data.data){
+      if (data && data.data) {
         sliders.value = data.data;
       }
     }
